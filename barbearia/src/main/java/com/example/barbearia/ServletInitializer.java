23a3fa0fb924334f90c785bbec1f9e9617999f0f
Stuurt <alisstu@outlook.com>
2024-06-21 22:04:19 -0300

    feat-get-hairs

---
+++ barbearia/src/main/java/com/example/barbearia/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.example.barbearia;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BarbeariaApplication.class);
+    }
+}
